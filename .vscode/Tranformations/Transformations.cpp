8b7832acc3663344378171800c1b638f07241c02
Ankush Shaw <shawank17198@gmail.com>
2018-03-04 18:14:35 +0530

    placevalue

---
+++ .vscode/Tranformations/Transformations.cpp
@@ -0,0 +1,22 @@
+
+
+
+std:tuple<2DObject,2DObject,2DObject> tranform3Dto2D(3Dobject object)
+{
+    return std::make_tuple(3DtoTop(object), 3DtoFront(object), 3DtoSide(object));
+}
+
+
+2DObject 3DtoTop (3Dobject object){
+
+}
+
+
+2DObject 3DtoFront (3Dobject object){
+
+}
+
+2DObject 3DtoSide (3Dobject object)
+{
+
+}
