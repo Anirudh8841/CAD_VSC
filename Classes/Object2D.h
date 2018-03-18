19fd4d7ad964070b102b1ebff1a8252a8775d003
Ankush Shaw <shawank17198@gmail.com>
2018-03-18 13:01:46 +0530

    Object2D.h deleted and Object.h renamed to 2d Object

--- Classes/Object2D.h
+++ Classes/Object2D.h
@@ -1,9 +1,19 @@
+#ifndef OBJECT2D_H
+#define OBJECT2D_H
+
 #include "Vertex.h"
 
-
-class Object{
+type std::vector<float>(3) Vertex;
+class Object2D{
     public: 
-        std::list<Vertex> VertexList;
-        std::list<std::tuple<Vertex, Vertex > > EdgeList;
+        std::tuple<int, int> FrontEdgeList;
+        std::vector<int> FrontHiddenVertex;
+        std::tuple<int, int> TopEdgeList;
+        std::vector<int> TopHiddenVertex;
+        std::tuple<int, int> SideEdgeList;
+        std::vector<int> SideHiddenVertex;
+        std::vector<Vertex> VertexList;
+
 };
 
+#endif
